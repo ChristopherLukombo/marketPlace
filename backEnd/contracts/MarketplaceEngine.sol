34313a4c8b5b671efca2a863e51cc4162e1e602b
Mickael Moreira <dopidot@gmail.com>
2019-12-04 16:20:33 +0100

    Fix issue with version 0.5.11 of Solidity

--- backEnd/contracts/MarketplaceEngine.sol
+++ backEnd/contracts/MarketplaceEngine.sol
@@ -1,4 +1,4 @@
-pragma solidity >= 0.4.21 < 0.6.0;
+pragma solidity >= 0.5.0 < 0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract MarketplaceEngine {
@@ -14,7 +14,7 @@ contract MarketplaceEngine {
         address salesman;
         uint256 roomCount;
         uint256 date;
-		bool isSold;
+        bool isSold;
     }
 
     // Dictionary idHouse with userAddress
@@ -23,17 +23,17 @@ contract MarketplaceEngine {
 
     function addHouse(
         string memory _addressHouse,
-        uint256 _price,
-        uint256 _surface,
-        string memory _description,
-        address[] memory _documents,
-        address _salesman,
+        uint256 _price, 
+        uint256 _surface, 
+        string memory _description, 
+        address[] memory _documents, 
+        address _salesman, 
         uint256 _roomCount,
         uint256 _date
         ) public {
-			
-        houses.push(House(houses.length + 1, _addressHouse, _price, _surface, _description, _documents, _salesman, _roomCount, _date, false));
-		owners[houses.length + 1] = msg.sender;	
+            
+            houses.push(House(houses.length + 1, _addressHouse, _price, _surface, _description, _documents, _salesman, _roomCount, _date, false));
+            owners[houses.length + 1] = msg.sender;
     }
 
     function buyHouse (uint256 _idHouse) external payable returns (bool)
@@ -44,7 +44,10 @@ contract MarketplaceEngine {
             {
                 require(msg.value == houses[i].price);
                 
-                owners[_idHouse].transfer(msg.value);
+                address addr1 = owners[_idHouse];
+                address payable walle = address(uint160(addr1));
+
+                walle.transfer(msg.value);
                 owners[_idHouse] = msg.sender; // Changement de proprio
                 houses[i].isSold = true;
                 
@@ -55,22 +58,24 @@ contract MarketplaceEngine {
         return false;
     }
     
-    function getSaleHouses() public returns (House[])
+    function getSaleHouses() public returns (House[] memory)
     {
-        House[] result;
+        House[] memory result;
+        uint index = 0;
         
         for(uint i = 0; i < houses.length; i++)
         {
             if (!houses[i].isSold)
             {
-                result.push(houses[i]);
+                result[index] = houses[i];
+                index++;
             }
         }
         
         return result;
     }
     
-    function getHouseInfo(uint256 _idHouse) public returns (House)
+    function getHouseInfo(uint256 _idHouse) public returns (House memory)
     {
         for(uint i = 0; i < houses.length; i++)
         {
