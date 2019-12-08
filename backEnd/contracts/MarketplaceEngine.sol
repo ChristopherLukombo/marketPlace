12d7500ce0b38a9789d5493589318289b8ca7612
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-12-08 01:06:26 +0100

    feat (MarketplaceEngine.sol): update functions in contract

--- backEnd/contracts/MarketplaceEngine.sol
+++ backEnd/contracts/MarketplaceEngine.sol
@@ -1,10 +1,9 @@
-pragma solidity >= 0.5.0 < 0.6.0;
+pragma solidity >=0.4.25 < 0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract MarketplaceEngine {
 
     struct House {
-
         uint256 idHouse;
         string addressHouse;
         uint256 price;
@@ -13,85 +12,103 @@ contract MarketplaceEngine {
         address[] documents;
         address salesman;
         uint256 roomCount;
-        uint256 date;
+        uint256 creationDate;
         bool isSold;
+        address owner;
     }
 
     // Dictionary idHouse with userAddress
 	mapping(uint256 => address) public owners;
     House[] public houses;
 
-    function addHouse(
-        string memory _addressHouse,
-        uint256 _price, 
-        uint256 _surface, 
-        string memory _description, 
-        address[] memory _documents, 
-        address _salesman, 
-        uint256 _roomCount,
-        uint256 _date
-        ) public {
-            
-            houses.push(House(houses.length + 1, _addressHouse, _price, _surface, _description, _documents, _salesman, _roomCount, _date, false));
-            owners[houses.length + 1] = msg.sender;
+    address transferTo;
+    address transferFrom;
+
+    event Transfer(address _transferTo, address _transferFrom, uint amount);
+
+    constructor() public {
+        transferFrom = msg.sender;
     }
 
-    function buyHouse (uint256 _idHouse) external payable returns (bool)
+    function addHouse(
+        string memory _addressHouse,
+        uint256 _price,
+        uint256 _surface,
+        string memory _description,
+        address[] memory _documents,
+        address _salesman,
+        uint256 _roomCount,
+        uint256 _creationDate,
+        address _owner
+        ) public {
+            houses.push(
+                House(
+                    houses.length,
+                    _addressHouse,
+                    _price,
+                    _surface,
+                    _description,
+                    _documents,
+                    _salesman,
+                    _roomCount,
+                    _creationDate,
+                    false,
+                    _owner));
+    }
+
+    function buyHouse(uint256 _idHouse) external payable returns (bool)
     {
-        for(uint i = 0; i < houses.length; i++)
+        for (uint i = 0; i < houses.length; i++)
         {
             if (houses[i].idHouse == _idHouse)
             {
-                require(msg.value == houses[i].price);
-                
-                address addr1 = owners[_idHouse];
-                address payable walle = address(uint160(addr1));
-
-                walle.transfer(msg.value);
-                owners[_idHouse] = msg.sender; // Changement de proprio
+                require((msg.value - houses[i].price) > 0, "Le propriétaire doit disposé de suffisament d'argent");
+                transferTo = houses[i].owner;
                 houses[i].isSold = true;
-                
+                transferTo.transfer(msg.value);
+                emit Transfer(transferTo, transferFrom, msg.value);
+                houses[i].owner = msg.sender; // Changement de proprio
                 return true;
             }
         }
-        
+
         return false;
     }
-    
-    function getSaleHouses() public returns (House[] memory)
+
+    function getSaleHouses() public view returns (House[] memory)
     {
         uint count = 0;
         uint index = 0;
-        
-        for(uint i = 0; i < houses.length; i++)
+
+        for (uint i = 0; i < houses.length; i++)
         {
             if (!houses[i].isSold)
             {
                 count++;
             }
         }
-        
+
         House[] memory result = new House[](count);
-        
-        for(uint i = 0; i < houses.length; i++)
+
+        for (uint j = 0; j < houses.length; j++)
         {
-            if (!houses[i].isSold)
+            if (!houses[j].isSold)
             {
-                result[index] = houses[i];
+                result[index] = houses[j];
                 index++;
             }
         }
-        
+
         return result;
     }
-    
-    function getHouseInfo(uint256 _idHouse) public returns (House memory)
+
+    function getHouseInfo(uint256 _idHouse) public view returns (House memory)
     {
-        for(uint i = 0; i < houses.length; i++)
+        for (uint k = 0; k < houses.length; k++)
         {
-            if (houses[i].idHouse == _idHouse)
+            if (houses[k].idHouse == _idHouse)
             {
-                return houses[i];
+                return houses[k];
             }
         }
     }
