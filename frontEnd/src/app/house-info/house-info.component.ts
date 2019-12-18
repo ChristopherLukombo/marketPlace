5bf5c8985542809b61c562fb1d0600173721ca73
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-12-19 00:22:27 +0100

    feat : documents and permit to download

--- frontEnd/src/app/house-info/house-info.component.ts
+++ frontEnd/src/app/house-info/house-info.component.ts
@@ -3,6 +3,9 @@ import { House } from 'src/model/house';
 import { ContractService } from '../services/contract.service';
 import { Web3Service } from '../services/web3.service';
 import { ActivatedRoute } from '@angular/router';
+import { saveAs } from 'file-saver';
+import { IpfsService } from '../services/ipfs.service';
+import { Material } from 'src/model/model.material';
 
 @Component({
   selector: 'app-house-info',
@@ -11,14 +14,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HouseInfoComponent implements OnInit {
 
-  house: any;
-
   from: string;
+
+  house: any;
+  materials: Array<Material>;
 
   constructor(
     private contractService: ContractService,
     private web3Service: Web3Service,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private ipfsService: IpfsService
   ) { }
 
 
@@ -41,10 +46,31 @@ export class HouseInfoComponent implements OnInit {
   }
 
   private getHouseInfo(_idHouse: number, account: string) {
-    this.contractService.getHouseInfo(_idHouse, account).subscribe(data => {
-      this.house = data;
-      this.house.creationDate = new Date(this.house.creationDate * 1000);
-    }, error => console.log('error getHouseInfo', error));
+    this.contractService.getHouseInfo(_idHouse, account)
+      .subscribe(data => {
+        this.house = data;
+        this.house.creationDate = new Date(this.house.creationDate * 1000);
+        this.findMaterialsByIdHouse(this.house.idHouse, this.from);
+      }, error => console.log('error getHouseInfo', error));
+  }
+
+  private findMaterialsByIdHouse(_idHouse: number, account: string) {
+    this.contractService.findMaterialsByIdHouse(_idHouse, account)
+      .subscribe(data => {
+        this.materials = data;
+      }, error => alert('error download'));
+  }
+
+  public downloadMaterial(fileName: string, fileHash: string): void {
+    this.ipfsService.getFile(fileHash)
+      .subscribe(data => {
+        // create a blob
+        const blob = new Blob([data[0].content.buffer]);
+        // download the document
+        saveAs(blob, fileName);
+      }, error => {
+        alert('Une erreur s\'est produite durant le téléchargement');
+      });
   }
 
 }
